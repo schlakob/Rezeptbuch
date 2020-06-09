aa3ea30fd994e05be10020291f67612ac8e9df65
schlakob <38752017+schlakob@users.noreply.github.com>
2020-06-09 21:04:36 +0200

    fixed first refesh error

--- src/main.js
+++ src/main.js
@@ -8,14 +8,21 @@ import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 import "./assets/materialKit.scss";
 import { firestorePlugin } from 'vuefire';
+import {auth} from './firebase/auth' 
 
 Vue.use(VueMaterial)
 Vue.use(firestorePlugin)
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+let app;
+
+auth.onAuthStateChanged(user => {
+    if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+  }
+});
